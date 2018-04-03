94eb8ff4756302f5e93035fb7ec4c20b2ad170d8
VinaLx <vinaleux@gmail.com>
2018-04-04 02:12:45 +0800

    interpolated string

---
+++ include/sushi/ast/interpolated-string.h
@@ -0,0 +1,89 @@
+#ifndef SUSHI_AST_INTERPOLATED_STRING_H_
+#define SUSHI_AST_INTERPOLATED_STRING_H_
+
+#include "./expression/expression.h"
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace sushi {
+
+class InterpolatedString {
+  public:
+    struct InterpolatedNode {
+
+        virtual ~InterpolatedNode() {}
+
+        void Traverse(
+            std::function<void(const std::string &s)> fs,
+            std::function<void(const Expression &)> fe) {
+            Visit(fs, fe);
+            if (next != nullptr) {
+                next->Traverse(std::move(fs), std::move(fe));
+            }
+        }
+        std::unique_ptr<InterpolatedNode> next;
+
+        virtual void Visit(
+            std::function<void(const std::string &)> &,
+            std::function<void(const Expression &)> &) = 0;
+    };
+
+    void Append(std::string s) {
+        AppendNode(std::make_unique<PlainString>(std::move(s)));
+    };
+
+    void Append(std::unique_ptr<Expression> expr) {
+        AppendNode(std::make_unique<Interpolation>(std::move(expr)));
+    }
+
+    void Traverse(
+        std::function<void(const std::string &)> fs,
+        std::function<void(const Expression &)> fe) {
+        if (node_ != nullptr) {
+            node_->Traverse(std::move(fs), std::move(fe));
+        }
+    }
+
+  private:
+    struct PlainString : public InterpolatedNode {
+        PlainString(std::string s) : s(std::move(s)) {}
+
+      private:
+        virtual void Visit(
+            std::function<void(const std::string &)> &fs,
+            std::function<void(const Expression &)> &) override {
+            fs(s);
+        }
+
+        std::string s;
+    };
+
+    struct Interpolation : public InterpolatedNode {
+        Interpolation(std::unique_ptr<Expression> e) : expr(std::move(e)) {}
+        std::unique_ptr<Expression> expr;
+
+      private:
+        virtual void Visit(
+            std::function<void(const std::string &)> &,
+            std::function<void(const Expression &)> &fe) override {
+            fe(*expr);
+        };
+    };
+
+    void AppendNode(std::unique_ptr<InterpolatedNode> next) {
+        if (node_ == nullptr) {
+            node_ = std::move(next);
+            tail_ = node_.get();
+        } else {
+            tail_->next = std::move(next);
+            tail_ = tail_->next.get();
+        }
+    }
+    std::unique_ptr<InterpolatedNode> node_;
+    InterpolatedNode *tail_;
+};
+
+} // namespace sushi
+
+#endif // SUSHI_AST_INTERPOLATED_STRING_H_
