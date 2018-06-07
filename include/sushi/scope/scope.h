64b3f60400b0d5e13d84654dcae9251a7e17e9e9
jskyzero <jskyzero@outlook.com>
2018-06-07 23:02:51 +0800

    add introdution about scope part

--- include/sushi/scope/scope.h
+++ include/sushi/scope/scope.h
@@ -16,6 +16,15 @@ namespace sushi {
 /// Scope => IdentInfo
 /// Scope => Scope (outer scopes)
 
+/// what scope do ? 
+/// scope part is between the lexer part and code-generation part, it will 
+/// receive infomation from lexer part and offer some useful services to 
+/// code-generation part, in detail:
+/// 1.type-check part, as far as I know, now we don't do the type check works.
+/// 2.find-scope part, help to find {a. identifier, b. expression, c. program}'s
+/// scope. 
+
+
 class Scope {
   public:
     struct IdentInfo {
