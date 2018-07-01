a22a10c935e6c45b03395c552a7b1799c932f8ca
DaddyTrap <914670787@qq.com>
2018-07-01 18:36:22 +0800

    :construction: Rewriting scope check test

--- test/scope/statement.cpp
+++ test/scope/statement.cpp
@@ -1,128 +1,136 @@
 #include "util.h"
 
-TEST(StatementTest, TestAssignment) {
-    std::string source_code = "x = 1";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("x");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-}
+using namespace sushi::scope::test;
 
 TEST(StatementTest, TestVariableDef) {
-    std::string source_code = "define x = 1";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("x");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
+    VariableDefSuccess("define x = 1", { "x" });
+    VariableDefSuccess("define x = 1; define y = 1;", { "x", "y" });
+    VariableDefError("define x = 1; define x = 0", Error::Type::kRedefinedError);
 }
 
-TEST(StatementTest, TestFunctionDef) {
-    std::string source_code =
-        "define f(x: Int, y: String, z: Array Int):Int = \n"
-        "    define x1 = 1 \n"
-        "    return z[x]";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("f");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-    auto & func_stmt = program.statements[0];
-    auto & func_body = ((ast::FunctionDef *)func_stmt.get())->body;
-    auto func_scope = environment.LookUp(&func_body);
-    EXPECT_TRUE(func_scope != nullptr);
-    ident_info = func_scope->LookUp("x1");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == func_scope);
-    EXPECT_TRUE(func_scope->Outer() == top_scope);
-}
+// TEST(StatementTest, TestAssignment) {
+//     std::string source_code = "x = 1";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("x");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+// }
 
-TEST(StatementTest, TestIfStmt) {
-    std::string source_code =
-        "if x < 5:\n"
-        "  print x1\n"
-        "else:\n"
-        "  print x2";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("x");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-    auto & if_stmt = program.statements[0];
-    auto & true_body = ((ast::IfStmt *)if_stmt.get())->true_body;
-    auto & false_body = ((ast::IfStmt *)if_stmt.get())->false_body;
-    auto true_scope = environment.LookUp(&true_body);
-    auto false_scope = environment.LookUp(&false_body);
-    EXPECT_TRUE(true_scope != nullptr);
-    EXPECT_TRUE(false_scope != nullptr);
+// TEST(StatementTest, TestVariableDef) {
+//     std::string source_code = "define x = 1";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("x");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+// }
 
-    ident_info = true_scope->LookUp("x1");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == true_scope);
-    EXPECT_TRUE(true_scope->Outer() == top_scope);
+// TEST(StatementTest, TestFunctionDef) {
+//     std::string source_code =
+//         "define f(x: Int, y: String, z: Array Int):Int = \n"
+//         "    define x1 = 1 \n"
+//         "    return z[x]";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("f");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+//     auto & func_stmt = program.statements[0];
+//     auto & func_body = ((ast::FunctionDef *)func_stmt.get())->body;
+//     auto func_scope = environment.LookUp(&func_body);
+//     EXPECT_TRUE(func_scope != nullptr);
+//     ident_info = func_scope->LookUp("x1");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == func_scope);
+//     EXPECT_TRUE(func_scope->Outer() == top_scope);
+// }
 
-    ident_info = false_scope->LookUp("x2");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == false_scope);
-    EXPECT_TRUE(false_scope->Outer() == top_scope);
-}
+// TEST(StatementTest, TestIfStmt) {
+//     std::string source_code =
+//         "if x < 5:\n"
+//         "  print x1\n"
+//         "else:\n"
+//         "  print x2";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("x");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+//     auto & if_stmt = program.statements[0];
+//     auto & true_body = ((ast::IfStmt *)if_stmt.get())->true_body;
+//     auto & false_body = ((ast::IfStmt *)if_stmt.get())->false_body;
+//     auto true_scope = environment.LookUp(&true_body);
+//     auto false_scope = environment.LookUp(&false_body);
+//     EXPECT_TRUE(true_scope != nullptr);
+//     EXPECT_TRUE(false_scope != nullptr);
 
-TEST(StatementTest, TestReturnStmt) {
-    std::string source_code = "return x";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("x");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-}
+//     ident_info = true_scope->LookUp("x1");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == true_scope);
+//     EXPECT_TRUE(true_scope->Outer() == top_scope);
 
-TEST(StatementTest, TestSwitchStmt) {
-    std::string source_code =
-        "switch x\n"
-        "  case 0\n"
-        "    print x\n"
-        "  case 1\n"
-        "    print x\n"
-        "  default\n"
-        "    print y";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("x");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-}
+//     ident_info = false_scope->LookUp("x2");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == false_scope);
+//     EXPECT_TRUE(false_scope->Outer() == top_scope);
+// }
 
-TEST(StatementTest, TestForStmt) {
-    std::string source_code = "for i < 4: continue";
-    auto program = Parse(source_code).program;
-    auto environment = ScopeCheck(program);
-    auto top_scope = environment.LookUp(&program);
-    auto ident_info = top_scope->LookUp("i");
-    EXPECT_TRUE(ident_info != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope != nullptr);
-    EXPECT_TRUE(ident_info->defined_scope == top_scope);
-}
+// TEST(StatementTest, TestReturnStmt) {
+//     std::string source_code = "return x";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("x");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+// }
 
-TEST(StatementTest, LoopControlStmt) {
-    // pass
-}
+// TEST(StatementTest, TestSwitchStmt) {
+//     std::string source_code =
+//         "switch x\n"
+//         "  case 0\n"
+//         "    print x\n"
+//         "  case 1\n"
+//         "    print x\n"
+//         "  default\n"
+//         "    print y";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("x");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+// }
 
-TEST(StatementTest, TestExpression) {
-    // pass
-}
+// TEST(StatementTest, TestForStmt) {
+//     std::string source_code = "for i < 4: continue";
+//     auto program = Parse(source_code).program;
+//     auto environment = ScopeCheck(program);
+//     auto top_scope = environment.LookUp(&program);
+//     auto ident_info = top_scope->LookUp("i");
+//     EXPECT_TRUE(ident_info != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope != nullptr);
+//     EXPECT_TRUE(ident_info->defined_scope == top_scope);
+// }
+
+// TEST(StatementTest, LoopControlStmt) {
+//     // pass
+// }
+
+// TEST(StatementTest, TestExpression) {
+//     // pass
+// }
--- test/scope/util.h
+++ test/scope/util.h
@@ -5,3 +5,79 @@
 #include "gtest/gtest.h"
 
 using namespace sushi::scope;
+
+namespace std {
+
+inline std::ostream &
+operator<<(std::ostream &os, const std::vector<Error> &es) {
+    for (auto &te : es) {
+        os << te.ToString() << '\n';
+    }
+    return os;
+}
+
+} // namespace std
+
+namespace sushi {
+namespace scope {
+namespace test {
+
+// borrowed from test/parser/util.h
+inline parser::ParsingResult Parse(const std::string &s) {
+    std::istringstream iss(s);
+    lexer::Lexer lexer(iss, {"", 1, 1});
+    parser::Parser p(std::move(lexer));
+    return p.Parse();
+}
+
+inline void SimpleWithScopeCheckResult(
+    const std::string &source,
+    std::function<
+        void(const Scope *, const Environment &, const std::vector<Error> &)>
+        f) {
+    SCOPED_TRACE(source);
+    auto pr = Parse(source);
+    ASSERT_TRUE(pr.errors.empty()) << pr.errors;
+    Environment env;
+    auto errs = ScopeCheck(pr.program, env);
+    auto scope = env.LookUp(&pr.program);
+
+    ASSERT_TRUE(scope != nullptr);
+
+    f(scope, env, errs);
+}
+
+inline void VariableDefSuccess(
+    const std::string &source,
+    const std::vector<std::string> &expected_scope_ids) {
+    SimpleWithScopeCheckResult(
+        source, [&expected_scope_ids](
+                    const Scope *scope, const Environment &env,
+                    const std::vector<Error> &errs) {
+            ASSERT_TRUE(errs.empty()) << errs;
+            for (auto &id : expected_scope_ids) {
+                auto res = scope->LookUp(id);
+                ASSERT_TRUE(res != nullptr);
+                EXPECT_TRUE(res->defined_scope == scope);
+            }
+        });
+}
+
+inline void VariableDefError(const std::string &source, const Error::Type &err_type) {
+    SimpleWithScopeCheckResult(
+        source, [&err_type](const Scope *scope, const Environment &env,
+                    const std::vector<Error> &errs) {
+            bool found_err = false;
+            for (auto & err : errs) {
+                if (err.type == err_type) {
+                    found_err = true;
+                    break;
+                }
+            }
+            EXPECT_TRUE(found_err);
+        });
+}
+
+} // namespace test
+} // namespace scope
+} // namespace sushi
