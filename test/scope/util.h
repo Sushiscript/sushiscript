9aa8778f7bf84df81db0c3fde56e4b64e81cf805
DaddyTrap <914670787@qq.com>
2018-07-02 08:52:38 +0800

    :fire: Delete some unused functions in scope check test

--- test/scope/util.h
+++ test/scope/util.h
@@ -34,56 +34,6 @@ inline parser::ParsingResult Parse(const std::string &s) {
     return p.Parse();
 }
 
-inline void SimpleWithScopeCheckResult(
-    const std::string &source,
-    std::function<
-        void(const Scope *, const Environment &, const std::vector<Error> &)>
-        f) {
-    SCOPED_TRACE(source);
-    auto pr = Parse(source);
-    ASSERT_TRUE(pr.errors.empty()) << pr.errors;
-    Environment env;
-    auto errs = ScopeCheck(pr.program, env);
-    auto scope = env.LookUp(&pr.program);
-
-    ASSERT_TRUE(scope != nullptr);
-
-    f(scope, env, errs);
-}
-
-inline void VariableDefSuccess(
-    const std::string &source,
-    const std::vector<std::string> &expected_scope_ids) {
-    SimpleWithScopeCheckResult(
-        source, [&expected_scope_ids](
-                    const Scope *scope, const Environment &env,
-                    const std::vector<Error> &errs) {
-            ASSERT_TRUE(errs.empty()) << errs;
-            for (auto &id : expected_scope_ids) {
-                auto res = scope->LookUp(id);
-                ASSERT_TRUE(res != nullptr);
-                EXPECT_TRUE(res->defined_scope == scope);
-            }
-        });
-}
-
-inline void
-VariableDefError(const std::string &source, const Error::Type &err_type) {
-    SimpleWithScopeCheckResult(
-        source, [&err_type](
-                    const Scope *scope, const Environment &env,
-                    const std::vector<Error> &errs) {
-            bool found_err = false;
-            for (auto &err : errs) {
-                if (err.type == err_type) {
-                    found_err = true;
-                    break;
-                }
-            }
-            EXPECT_TRUE(found_err);
-        });
-}
-
 struct PretestResult {
     std::vector<const ast::Program *> programs;
     std::vector<const ast::Identifier *> identifiers;
